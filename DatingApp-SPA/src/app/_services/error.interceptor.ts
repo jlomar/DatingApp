794ce81dc0726432c19cebedaea8c2728162f750
joao <joaolomar1996@hotmail.com>
2019-10-16 22:14:28 +0100

    Error Handling

---
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -0,0 +1,43 @@
+import { HttpInterceptor, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    req: import('@angular/common/http').HttpRequest<any>,
+    next: import('@angular/common/http').HttpHandler
+  ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
+
+    return next.handle(req).pipe(
+    catchError(error => {
+      if (error.status === 401) {
+        return throwError (error.statusText);
+      }
+      if (error instanceof HttpErrorResponse) {
+        const aplicationError = error.headers.get('Application-Error');
+        if (aplicationError) {
+          return throwError(aplicationError);
+        }
+        const serverError = error.error;
+        let modalStateErrors = '';
+        if (serverError.errors && typeof serverError.errors === 'object') {
+          for (const key in serverError.errors) {
+            if (serverError.errors[key]) {
+              modalStateErrors += serverError.errors[key] + '\n';
+            }
+          }
+        }
+        return throwError(modalStateErrors || serverError || 'Server Error');
+      }
+    })
+    );
+  }
+}
+
+export const ErrorInterceptProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
+};
